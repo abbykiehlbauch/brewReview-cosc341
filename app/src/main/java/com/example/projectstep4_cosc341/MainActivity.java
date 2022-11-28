0293c040fbac1477a2c4bb7960e8d0feaf38d80a
Sierra Williams <williamssierra90@gmail.com>
2022-11-28 10:46:36 -0800

    updates

--- app/src/main/java/com/example/projectstep4_cosc341/MainActivity.java
+++ app/src/main/java/com/example/projectstep4_cosc341/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView welcome;
-    Button menu, profile;
+    Button menu;
+    Button profile_;
+    View profile;
     EditText search;
     Spinner sort;
     ListView breweries;
-    String breweryNames[] = {"Kelowna Brewing Company", "Shoreline Brewing", "Red Bird Brewing", "Bad Tattoo Brewing"};
-
+    String[] breweryNames = {"Kelowna Brewing Company", "Shoreline Brewing", "Red Bird Brewing", "Bad Tattoo Brewing"};
+    String[] sorting = {"SORT BY", "NEAR ME", "HIGHEST RATING", "LARGEST SELECTION"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +30,20 @@ public class MainActivity extends AppCompatActivity {
 
         welcome = findViewById(R.id.welcome);
         menu = findViewById(R.id.menu);
-        profile = findViewById(R.id.userProfile);
         search = findViewById(R.id.searchBar);
         sort = findViewById(R.id.sort);
         breweries = findViewById(R.id.breweriesList);
+        profile_ = findViewById(R.id.userProfile);
+//        profile.setVisibility(View.INVISIBLE);
 
-        Intent intent = new Intent(this, Listview.class);
-        startActivity(intent);
-
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, breweryNames);
-        //breweries.setAdapter(adapter);
+//        Intent intent = new Intent(this, BreweriesList.class);
+//        startActivity(intent);
+//        ArrayAdapter<String> sorted = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sorting);
+//        sorted.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        sort.setAdapter(sorted);
+//
+//        ArrayAdapter<String> list = new ArrayAdapter<String>(this, R.layout.activity_breweries_list, R.id.textView, breweryNames);
+//        breweries.setAdapter(list);
 
 
     }
