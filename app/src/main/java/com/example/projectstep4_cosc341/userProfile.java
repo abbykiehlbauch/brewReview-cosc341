ee61e4e75ce2c52e216af41e2f3e5866c04d9e90
LiamRHS <l.rae2000@hotmail.com>
2022-11-27 16:41:48 -0800

    started userProfile, added back and user button

--- app/src/main/java/com/example/projectstep4_cosc341/userProfile.java
+++ app/src/main/java/com/example/projectstep4_cosc341/userProfile.java
@@ -2,7 +2,9 @@ package com.example.projectstep4_cosc341;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class userProfile extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class userProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void backbtn(View view)
+    {
+        finish();
+    }
+
+    public void onProfileClick(View view)
+    {
+        Intent intent = new Intent(this, userProfile.class);
+        startActivity(intent);
     }
 }
