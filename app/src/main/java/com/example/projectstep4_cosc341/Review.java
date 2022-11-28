c9d704c9a8c7dd3e4fda32dccf93d531a2b5db91
LiamRHS <l.rae2000@hotmail.com>
2022-11-27 16:36:38 -0800

    changed rating to starRating and made it an int

--- app/src/main/java/com/example/projectstep4_cosc341/Review.java
+++ app/src/main/java/com/example/projectstep4_cosc341/Review.java
@@ -1,16 +1,16 @@
 package com.example.projectstep4_cosc341;
 
 public class Review {
-    String brewery, rating, reviewText;
-
+    String brewery,reviewText;
+    int starRating;
     public Review(){
         brewery=null;
-        rating=null;
+        starRating=0;
         reviewText=null;
     }
-    public Review(String brewery, String rating, String reviewText){
+    public Review(String brewery, int starRating, String reviewText){
         this.brewery=brewery;
-        this.rating=rating;
+        this.starRating=starRating;
         this.reviewText=reviewText;
     }
 }
