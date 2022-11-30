5290461d994061fdcf343e1a25c3a8db7fdd7530
Sierra Williams <williamssierra90@gmail.com>
2022-11-29 17:00:37 -0800

    Main finished, onClick, sort, and search done

--- app/src/main/java/com/example/projectstep4_cosc341/Listview.java
+++ app/src/main/java/com/example/projectstep4_cosc341/Listview.java
@@ -15,8 +15,14 @@ public class Listview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
         Intent intent = getIntent();
-        //Bundle bundle = intent.getExtras();
+        Bundle bundle = intent.getExtras();
+
+        bundle.getString("distance");
+        bundle.getString("rating");
+        bundle.getString("selection");
+        bundle.getString("description");
         TextView breweryName = findViewById(R.id.breweryName);
+        breweryName.setText( bundle.getString("name"));
         //breweryName.setText(bundle.getString("BreweryName"));
         final RatingBar myRatingBar = (RatingBar) findViewById(R.id.MyRating);
         myRatingBar.setRating(4);
